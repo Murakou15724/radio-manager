ff1b5abb31a696bd333cf5eefc5bdf28eca41261
LBZmurakami <murakami.ko@laiblitz.co.jp>
2026-04-26 19:12:07 +0900

    データベースの実装

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2026_04_24_000000) do
-  create_table "programs", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "programs", force: :cascade do |t|
     t.string "name"
     t.integer "frequency_type"
     t.integer "weekday"
